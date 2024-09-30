cc0e837916c5a705c8884497dca975387ffa750c
Samuel Gomes <sam2022gs@gmail.com>
2024-09-30 20:54:33 -0300

    Criando os chats

--- src/components/ModalCreateGP/index.js
+++ src/components/ModalCreateGP/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
     Container,
     TransparenteView,
@@ -10,11 +10,46 @@ import {
     AreaInput
 } from "./styles";
 import { TouchableWithoutFeedback } from "react-native";
+import { Context } from "../../context/context";
+
+import firestore from "@react-native-firebase/firestore";
 
 
 export default function ModalCreateGP({ setVisible }) {
 
+    const { user } = useContext(Context)
+
     const [nameChat, setNameChat] = useState('')
+
+    function handleButtonCreate() {
+        if (nameChat === '') {
+            return
+        }
+        handleCreateRoom()
+    }
+
+
+    async function handleCreateRoom() {
+        await firestore().collection('MESSAGE_THREADS').add({
+            name: nameChat,
+            owner: user.uid,
+            lastMessage: {
+                text: `Grupo ${nameChat} criado. Seja bem vindo(a)!`,
+                createdAt: firestore.FieldValue.serverTimestamp(),
+
+            }
+        }).then((docRef) => {
+            docRef.collection('MESSAGES').add({
+                text: `Grupo ${nameChat} criado. Seja bem vindo(a)!`,
+                createdAt: firestore.FieldValue.serverTimestamp(),
+                system: true, //Para saber que a mensagem foi criada pelo systema.F
+            }).then(() => setVisible())
+
+
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
 
 
     return (
@@ -33,7 +68,7 @@ export default function ModalCreateGP({ setVisible }) {
                         onChangeText={(text) => setNameChat(text)}
                     />
                 </AreaInput>
-                <CreateButtonGP onPress={() => setVisible()}>
+                <CreateButtonGP onPress={() => handleButtonCreate()}>
                     <ButtonText> Criar sala </ButtonText>
                 </CreateButtonGP>
             </ContentActions>
